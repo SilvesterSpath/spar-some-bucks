9029024a07851b422a1fb806ea7c64609f0f36e1
SilvesterSpath <spath.sz@gmail.com>
2021-12-28 07:32:35 +0100

    scroll

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const menu = document.getElementById('menu');
 function navToggle() {
   button.classList.toggle('open');
   menu.classList.toggle('hidden');
+  document.body.classList.toggle('no-scroll');
 }
 
 button.addEventListener('click', navToggle);
